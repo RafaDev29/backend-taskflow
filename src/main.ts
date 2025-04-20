bb593cc876b74a3ab2b9741c6eca4588bba75c49
Rafael Rossel Galarza <rosselgalarzarafael@gmail.com>
2025-04-19 19:37:10 -0500

    se añadio los guards

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Activar CORS
+  app.enableCors({
+    origin: '*', // Puedes reemplazar '*' por dominios específicos si lo deseas
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api/v1');
   app.useGlobalInterceptors(new ResponseInterceptor());
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
